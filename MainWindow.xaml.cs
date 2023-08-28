ae9b77210f87f79512125ad491e49f5ac223ee4d
Kamil Biden <Kamil Biden@DESKTOP-2MB9CB8>
2023-08-28 11:03:50 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace notatnik
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItemNewWindow_Click(object sender, RoutedEventArgs e)
+        {
+            var n1 = new MainWindow();
+            n1.Show();
+        }
+
+        private void mainInputField_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string str = mainInputField.Text;
+            int occur = 0;
+            int occur_verbs = 0;
+            int x = 0;
+
+            foreach (char letter in str)
+            {
+                occur++;
+            }
+
+            while (x <= str.Length - 1)
+            {
+                if (str[x] == ' ' || str[x] == '\n' || str[x] == '\t')
+                {
+                    occur_verbs++;
+                }
+                x++;
+            }
+
+            count_signs.Text = "Ilość znaków: " + occur;
+            count_verbs.Text = "Ilość wyrazów: " + occur_verbs;
+        }
+
+        private void MenuItemClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MenuItemOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text file (*.txt)|*.txt";
+            if (openFileDialog.ShowDialog() == true)
+                mainInputField.Text = File.ReadAllText(openFileDialog.FileName);
+        }
+
+        private void MenuItemSaveFileAs_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text file (*.txt)|*.txt";
+            if (saveFileDialog.ShowDialog() == true)
+                File.WriteAllText(saveFileDialog.FileName, mainInputField.Text);
+        }
+
+        private void MenuItemTextWrapping_Click(object sender, RoutedEventArgs e)
+        {
+            if (mainInputField.TextWrapping == TextWrapping.Wrap)
+            {
+                mainInputField.TextWrapping = TextWrapping.NoWrap;
+                textwrapbutton.IsChecked = false;   
+            }
+            else
+            {
+                textwrapbutton.IsChecked = true;   
+                mainInputField.TextWrapping = TextWrapping.Wrap;
+            }
+        }
+
+        private void MenuItemChooseFont_Click(object sender, RoutedEventArgs e)
+        {
+            var choice = new wybor_czcionki();
+            choice.Show();
+          
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            mainInputField.FontSize += 5;
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            mainInputField.FontSize -= 5;
+
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Radź sobie","Pomoc");
+        }
+    }
+}
---
+++ wybor_czcionki.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace notatnik
+{
+    /// <summary>
+    /// Logika interakcji dla klasy wybor_czcionki.xaml
+    /// </summary>
+    public partial class wybor_czcionki : Window
+    {
+        public wybor_czcionki()
+        {
+            InitializeComponent();
+        }
+
+        private void listbox_fonts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (listbox_fonts.SelectedIndex == 0 && listbox_fonts.SelectedIndex != -1)
+            {
+                sampletext.FontFamily = new FontFamily("Cooper");
+            }
+            else if (listbox_fonts.SelectedIndex == 1 && listbox_fonts.SelectedIndex != -1)
+            {
+                sampletext.FontFamily = new FontFamily("Arial");
+            }
+            else if (listbox_fonts.SelectedIndex == 2 && listbox_fonts.SelectedIndex != -1)
+            {
+                sampletext.FontFamily = new FontFamily("Calibri");
+            }
+            else if (listbox_fonts.SelectedIndex == 3 && listbox_fonts.SelectedIndex != -1)
+            {
+                sampletext.FontFamily = new FontFamily("Impact");
+            }
+            else if (listbox_fonts.SelectedIndex == 4 && listbox_fonts.SelectedIndex != -1)
+            {
+                sampletext.FontFamily = new FontFamily("Jokerman");
+            }
+
+        }
+
+        private void listbox_size_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (listbox_size.SelectedIndex == 0 && listbox_size.SelectedIndex != -1)
+            {
+                sampletext.FontSize = 8;
+            }
+            else if (listbox_size.SelectedIndex == 1 && listbox_size.SelectedIndex != -1)
+            {
+                sampletext.FontSize = 12;
+            }
+            else if (listbox_size.SelectedIndex == 2 && listbox_size.SelectedIndex != -1)
+            {
+                sampletext.FontSize = 16;
+            }
+            else if (listbox_size.SelectedIndex == 3 && listbox_size.SelectedIndex != -1)
+            {
+                sampletext.FontSize = 18;
+            }
+            else if (listbox_size.SelectedIndex == 4 && listbox_size.SelectedIndex != -1)
+            {
+                sampletext.FontSize = 22;
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+           // ((MainWindow)Application.Current.MainWindow).mainInputField.FontFamily = 
+           string selectedFont = ((ListBoxItem)listbox_fonts.SelectedValue).Content.ToString();
+           string selectedSize = ((ListBoxItem)listbox_size.SelectedValue).Content.ToString();
+
+            ((MainWindow)Application.Current.MainWindow).mainInputField.FontFamily = new FontFamily(selectedFont);
+            ((MainWindow)Application.Current.MainWindow).mainInputField.FontSize = int.Parse(selectedSize);
+        }
+    }
+}
